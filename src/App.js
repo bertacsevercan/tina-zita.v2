4a8cdf6a42baa81a7dfd7ecf4320aa40e73f3a62
bertacsevercan <bertac.severcan@gmail.com>
2020-10-18 22:19:14 +0300

    Fixed the minHeight for content

--- src/App.js
+++ src/App.js
@@ -22,9 +22,9 @@ function App() {
       <Layout>
         <SideBar/>
       <Layout>
-      <Header className="site-layout-sub-header-background" style={{ padding: 0 }} />
+      
       <Content style={{ margin: '24px 16px 0' }}>
-        <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
+        <div className="site-layout-background" style={{ padding: 24, minHeight: "88vh" }}>
         
         <Route exact path="/" component={Admin} />
         <Route path="/inventory" component={Inventory} />
--- src/containers/Admin/index.jsx
+++ src/containers/Admin/index.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 const Admin = () => {
     return(
+      
         <h1>Admin page here!</h1>
+       
     )
 }
 export default Admin;
