7c4a5dd2f1b2d08517c79674f3f139a8625543da
bertacsevercan <bertac.severcan@gmail.com>
2020-10-18 23:33:40 +0300

    Tried some db

--- src/containers/Recipe/index.jsx
+++ src/containers/Recipe/index.jsx
@@ -1,6 +1,7 @@
-import React from "react";
-
+import React, { useEffect } from "react";
+import db from "../../firebaseConfig"
 const Recipe = () => {
+   
     return(
         <h1>Recipe page here!</h1>
     )
