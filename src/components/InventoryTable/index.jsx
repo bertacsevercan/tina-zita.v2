6d947eeea3455a613cc60eef61480afdb9b54c40
bertacsevercan <bertac.severcan@gmail.com>
2020-10-19 17:28:49 +0300

    Added search in InventoryTable

--- src/components/InventoryTable/index.jsx
+++ src/components/InventoryTable/index.jsx
@@ -1,54 +1,137 @@
-import React from "react";
-import { Table, Button, Space } from 'antd';
-const InventoryTable = ({item}) => {
+import React, { useState } from "react";
+import { Table, Button,Input, Space } from 'antd';
+import { SearchOutlined } from '@ant-design/icons';
+import Highlighter from 'react-highlight-words';
 
-    const columns = [
-        {
-            title: 'Name',
-            dataIndex: 'itemName',
-            key: 'itemName',
-            
-           
-          },
-          {
-            title: 'Code',
-            dataIndex: 'itemCode',
-            key: 'itemCode',
-          },
-          {
-            title: 'Category',
-            dataIndex: 'category',
-            key: 'category',
-          },
-          {
-            title: 'Unit',
-            key: 'measurementUnit',
-            dataIndex: 'measurementUnit',
-            
-          },
-          {
-            title: 'Price',
-            key: 'price',
-            dataIndex: 'price',
-            
-          },
-          {
-            title: 'Stock',
-            key: 'stock',
-            dataIndex: 'stock',
-            
-          },
-          {
-            title: 'Action',
-            key: 'action',
-            render: () => (
-              <Space>
-                <Button type="primary">Edit </Button>
-                <Button type="primary" danger> Delete</Button>
-              </Space>
-            ),
-          },
-        ];
+const InventoryTable = ({item}) => {
+  const [search, setSearch] = useState({
+    searchText: '',
+    searchedColumn: '',
+  });
+
+  let searchInput;
+
+    const getColumnSearchProps = dataIndex => ({
+      filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+        <div style={{ padding: 8 }}>
+          <Input
+            ref={node => {
+              searchInput = node;
+            }}
+            placeholder={`Search ${dataIndex}`}
+            value={selectedKeys[0]}
+            onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+            onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            style={{ width: 188, marginBottom: 8, display: 'block' }}
+          />
+          <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+            icon={<SearchOutlined />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button onClick={() => handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+            Reset
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: filtered => <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />,
+    onFilter: (value, record) =>
+      record[dataIndex]
+        ? record[dataIndex].toString().toLowerCase().includes(value.toLowerCase())
+        : '',
+    onFilterDropdownVisibleChange: visible => {
+      if (visible) {
+        setTimeout(() => searchInput.select(), 100);
+      }
+    },
+    render: text =>
+      search.searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[search.searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
+
+  const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearch({
+      searchText: selectedKeys[0],
+      searchedColumn: dataIndex,
+    });
+  };
+
+  const handleReset = clearFilters => {
+    clearFilters();
+    setSearch({ searchText: '' });
+  };
+  const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'itemName',
+        key: 'itemName',
+        ...getColumnSearchProps('itemName')
+        
+       
+      },
+      {
+        title: 'Code',
+        dataIndex: 'itemCode',
+        key: 'itemCode',
+        ...getColumnSearchProps('itemCode')
+      },
+      {
+        title: 'Category',
+        dataIndex: 'category',
+        key: 'category',
+        ...getColumnSearchProps('category')
+      },
+      {
+        title: 'Unit',
+        key: 'measurementUnit',
+        dataIndex: 'measurementUnit',
+        ...getColumnSearchProps('measurementUnit')
+        
+      },
+      {
+        title: 'Price',
+        key: 'price',
+        dataIndex: 'price',
+        ...getColumnSearchProps('price')
+
+        
+      },
+      {
+        title: 'Stock',
+        key: 'stock',
+        dataIndex: 'stock',
+        ...getColumnSearchProps('stock')
+
+        
+      },
+      {
+        title: 'Action',
+        key: 'action',
+        render: () => (
+          <Space>
+            <Button type="primary">Edit </Button>
+            <Button type="primary" danger> Delete</Button>
+          </Space>
+        ),
+      },
+    ];
+
+
 
 
 
--- src/containers/Inventory/index.jsx
+++ src/containers/Inventory/index.jsx
@@ -24,9 +24,6 @@ const Inventory = () => {
       
     }, []);
     
-
-
-
     return(
         <div>
 
