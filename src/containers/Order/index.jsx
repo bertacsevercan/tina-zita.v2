25deb2ff3286bb0868ed0ab664a2ac822f9d9e10
ihab-sensei <ib-mashhadani@hotmail.com>
2020-10-20 20:55:10 +0300

    Added ordering functionality

--- src/containers/Order/index.jsx
+++ src/containers/Order/index.jsx
@@ -1,10 +1,92 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import SelectOrder from "../../components/SelectOrder/SelectOrder"
+import { Button, Input } from 'antd';
+import db from "../../firebaseConfig"
 const Order = () => {
-    return(
-        <h1>Order page here!</h1>
-    )
+  const [orders, setOrders] = useState([])
+  const [orderMultiplier, setOrderMultiplier] = useState(1)
+  const [selectedOrder, setSelectedOrder] = useState("")
+  const fetchOrders = async () => {
+    const res = await db.collection("recipe").get()
+    const data = res.docs.map(doc => doc.data());
+    const data2 = data.map(obj => {
+      return {label: obj.orderName, value:obj.orderCode};
+    })
+    console.log("data",data.find((order) => order.orderCode === "OSAL").ingredients);
+    setOrders(data)
+
+  }
+
+  const addOrder = async() => {
+    if (orders.length > 0){
+      let isSufficient = true
+      const ingredientsArr = orders
+      .find((order) => order.orderCode === selectedOrder)
+      .ingredients;
+      
+      ingredientsArr.forEach(async(orderItem) => {
+      const res = await orderItem.itemDocRef.get()
+    const data = res.data()
+    console.log(data.stock);
+    if(data.stock - orderItem.requiredAmount * orderMultiplier < 0) {
+      isSufficient = false
+      console.log(isSufficient);
+    }
+    })
+    setTimeout(()=> {
+
+      if (isSufficient) {
+        console.log("i'm sufficient");
+        ingredientsArr.forEach(async(orderItem) => {
+          const res = await orderItem.itemDocRef.get()
+          const data = res.data()
+          console.log(data.stock);
+          orderItem.itemDocRef.update({
+            stock: data.stock - orderItem.requiredAmount * orderMultiplier
+          })
+        });
+        
+      } else {
+        alert("insufficient ingredients")
+      }
+
+    },1000)
+    
+
+
+      
+      
+    } else {
+      console.log("empty");
+    }
+  }
+
+  function onChange(value) {
+      setSelectedOrder(value);
+    }
+    
+  
+    useEffect(() => {
+      fetchOrders();
+      
+    },[]);
+  return(
+      <div >
+      <h1>Order page here!</h1>
+      <div style={{display:"flex", justifyContent: "center"}}>
+      <SelectOrder 
+      onChange={onChange} 
+      orders={orders}/>
+      <Input style={{width: "200px"}} type="number" onChange={(e) => setOrderMultiplier(e.target.value)} value={orderMultiplier} placeholder="number of orders" min={1}/>
+      <Button disabled={selectedOrder? false : true} onClick={addOrder} type="primary">Add Order</Button>
+      </div>
+      </div>
+  )
 }
+
+export default Order;
+
+
 /* const fetchData = async () => {
         const res = await db.collection("recipe").doc("RSAL").get()
         const data = res.data()
@@ -22,4 +104,7 @@ const Order = () => {
     },[])
  */
 
-export default Order;
+
+
+
+
