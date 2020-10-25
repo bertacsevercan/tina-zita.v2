96c2bf609d3ee1a13b0fc6c130a49a0acdb27cb8
fadime94 <fadime.ozdemir11@gmail.com>
2020-10-25 17:04:08 +0300

    finish log in/ log out and reset password

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -6,13 +6,25 @@ import { Typography } from 'antd';
 
 const { Text, Title } = Typography;
 
-const Home = ()=>{
+const Home = ({email,setEmail, password,isEmailSend, setIsEmailSend, setPassword,handleLogin,handleResetPassword,hasPassword,setHasPassword,emailError,passwordError})=>{
     return(
         <div className="home">
             <div className="hero-text">
-            <Title><h1 >TINA ZITA'S INVENTORY</h1></Title>
+            <Title>TINA ZITA'S INVENTORY</Title>
                 <p>Please insert Tina Zita's credentials</p>
-                <Login />
+                <Login
+               email={email}
+               setEmail={setEmail}
+               password={password}
+               setPassword={setPassword}
+               handleLogin={handleLogin}
+               handleResetPassword={handleResetPassword}
+               hasPassword={hasPassword}
+               setHasPassword={setHasPassword}
+               emailError={emailError}
+               passwordError={passwordError}
+               isEmailSend={isEmailSend}
+               />
                 <Text type="secondary">Don't you have credentials? Ask <Text type="danger">Rawan</Text></Text>
             </div>
             <img src="./image/inventory.svg" alt="inventory" />
