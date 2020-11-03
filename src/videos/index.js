455c738f6579f78c65c6c43e5072a406514cf910
bertacsevercan <bertac.severcan@gmail.com>
2020-11-03 13:32:12 +0300

    add allowfullscreen to videos

--- src/videos/index.js
+++ src/videos/index.js
@@ -6,35 +6,35 @@ const videoList = [
     src="https://www.youtube.com/embed/99R0FPQwAjk" 
     frameborder="0" 
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-    allowfullscreen></iframe>,
+    allowFullScreen></iframe>,
     
     <iframe width="560" height="315"
     title="generalLayout"
     src="https://www.youtube.com/embed/XPHBn2mckTo" 
     frameborder="0" 
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-    allowfullscreen></iframe> ,
+    allowFullScreen></iframe> ,
 
     <iframe width="560" height="315" 
     title="inventoryDetail"
     src="https://www.youtube.com/embed/njfiKRixlWo"
     frameborder="0" 
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-    allowfullscreen></iframe>,
+    allowFullScreen></iframe>,
 
     <iframe width="560" height="315"
     title="recipeDetail"
     src="https://www.youtube.com/embed/UylLaMnT5S8" 
     frameborder="0" 
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-    allowfullscreen></iframe>,
+    allowFullScreen></iframe>,
 
     <iframe width="560" height="315"
     title="orderDetail"
     src="https://www.youtube.com/embed/PsXdRnJxFBU" 
     frameborder="0" 
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-    allowfullscreen></iframe>
+    allowFullScreen></iframe>
 ]
 
 export default videoList;
