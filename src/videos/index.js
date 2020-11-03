1e24ab42266910149b85cef70f21b7ced4b9878e
ihab-sensei <ib-mashhadani@hotmail.com>
2020-11-03 14:45:59 +0300

    squash bugs and adjustments

--- src/videos/index.js
+++ src/videos/index.js
@@ -1,26 +1,35 @@
 import React from "react";
 
 const videoList = [
-    <iframe width="560" height="315" 
+  <iframe
+    width="560"
+    height="315"
     title="loginScreen"
-    src="https://www.youtube.com/embed/99R0FPQwAjk" 
-    frameborder="0" 
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-    allowfullscreen></iframe>,
-    
-    <iframe width="560" height="315"
-    title="generalLayout"
-    src="https://www.youtube.com/embed/XPHBn2mckTo" 
-    frameborder="0" 
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-    allowfullscreen></iframe> ,
+    src="https://www.youtube.com/embed/99R0FPQwAjk"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>,
 
-    <iframe width="560" height="315" 
+  <iframe
+    width="560"
+    height="315"
+    title="generalLayout"
+    src="https://www.youtube.com/embed/XPHBn2mckTo"
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>,
+
+  <iframe
+    width="560"
+    height="315"
     title="inventoryDetail"
     src="https://www.youtube.com/embed/njfiKRixlWo"
-    frameborder="0" 
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-    allowfullscreen></iframe>
-]
+    frameborder="0"
+    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen
+  ></iframe>,
+];
 
 export default videoList;
