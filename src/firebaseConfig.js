7059cfc99f9b434a77f469ced0a10f9bb860f499
ihab-sensei <ib-mashhadani@hotmail.com>
2020-11-04 18:34:31 +0300

    wrap reverse, firebase

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,17 +1,17 @@
-import * as firebase from 'firebase';
-import 'firebase/database';
+import { firestore, auth, initializeApp } from "firebase";
+import "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCpUk4RtMohc1fg4WoljZUZrLGDWVfrlyU",
-    authDomain: "tina-zita-2.firebaseapp.com",
-    databaseURL: "https://tina-zita-2.firebaseio.com",
-    projectId: "tina-zita-2",
-    storageBucket: "tina-zita-2.appspot.com",
-    messagingSenderId: "529532594205",
-    appId: "1:529532594205:web:24a37b81025e0b17fc7ef4"
-  };
-firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyCpUk4RtMohc1fg4WoljZUZrLGDWVfrlyU",
+  authDomain: "tina-zita-2.firebaseapp.com",
+  databaseURL: "https://tina-zita-2.firebaseio.com",
+  projectId: "tina-zita-2",
+  storageBucket: "tina-zita-2.appspot.com",
+  messagingSenderId: "529532594205",
+  appId: "1:529532594205:web:24a37b81025e0b17fc7ef4",
+};
+initializeApp(firebaseConfig);
 
-export default firebase.firestore();
+export default firestore();
 
-export const Auth = firebase.auth;
+export const Auth = auth;
